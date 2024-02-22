23657acb7c8e014dd9ed9589a4c6864202811c31
Yasin_YATKIN <yatkinyasin20@gmail.com>
2024-02-23 00:55:30 +0300

    22.02.2024_son_guncelleme

--- deneme/deneme/Program.cs
+++ deneme/deneme/Program.cs
@@ -17,9 +17,6 @@ namespace deneme
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            deneme1
-                yasin
-                yatkın
         }
     }
 }
